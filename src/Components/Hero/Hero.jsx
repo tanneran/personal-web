797b1d21428f298e6b6fb267a61bf9d8bb972b29
alinaaaaan <anhuiyi216@gmail.com>
2024-09-28 01:56:20 -0400

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Hero.scss'
+import profile_img from '../../assets/profile_img_huiyi_2.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+                <img src={profile_img} alt="Profile image"/>
+
+            <h1><span>I'm Tanner Huiyi An</span></h1>
+            <p>I have more than 3 years of professional experience. I provided services in product design, UIUX design, product engineering, project management, marketing, and sales.</p>
+            <div className="hero-action">
+                <div className="hero-connect">
+                    <AnchorLink className='anchor-link' offset={90} href='#contact'>Make magic with Me
+                    </AnchorLink>
+                </div>
+                <div className="hero-resume" onClick={()=> {
+                    const url='https://drive.google.com/file/d/11Lj5lmbxKP6U2ZNg-jHlV_xn50NXFo1g/view?usp=sharing'
+                    window.open(url,'_blank')
+                }}>
+                    My resume
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,62 @@
+import project1_img from '../assets/project_BCC3-01.png'
+import project2_img from '../assets/project_drayeasy.svg'
+import project3_img from '../assets/project_JCSB-01.png'
+import project4_img from '../assets/project_Pie-01.png'
+import project5_img from '../assets/project_swivel.svg'
+import project6_img from '../assets/project_6.svg'
+import project0_img from '../assets/project_portfolio.svg'
+
+
+const mywork_data = [
+    {
+        w_no:0,
+        w_name: "TannerAn-portfolio",
+        w_img:project0_img,
+        w_desc: "Built a portfolio website independently using Vite with React.js"
+    },
+    // {
+    //     w_no:1,
+    //     w_name:"Baltimore Code&Coffee",
+    //     w_img:project1_img,
+    //     w_desc: "Managed a project from scratch to launch for a coding organization",
+    //     w_url: "https://www.figma.com/design/9gmylSmBa1DAzJxan97Ryv/Baltimore-Code-%26-Coffee-Website?node-id=0-1&t=9f9sFC9Z4sgyNQvy-1"
+    // },
+    {
+        w_no:1,
+        w_name:"DrayEasy",
+        w_img:project2_img,
+        w_desc: "Supercharged a B2B web app for one of US's best drayage pricing platforms ",
+        w_url: "https://www.figma.com/proto/SkKYgKqUs4pKuQbqFD8p51/DrayEasy---Rate-Review?node-id=3408-87072&node-type=frame&t=X1Te7cIg2GIorVz6-0&scaling=scale-down&content-scaling=fixed&page-id=3283%3A82123&starting-point-node-id=3408%3A87072"
+    },
+    {
+        w_no:3,
+        w_name:"Jonathan Ceci Landscape Architects",
+        w_img:project3_img,
+        w_desc: "Led marketing and ensured client satisfaction in a niche design firm",
+        w_url: "https://www.jonathanceci.com/"
+    },
+    {
+        w_no:4,
+        w_name:"Pie Properties",
+        w_img:project4_img,
+        w_desc: "Revamped the design of a crowdfunding platform for real estate investments",
+        w_url: "https://www.figma.com/proto/LF7cwVMb8LnzqzBsvsvRnC/Pie-Properties---Alpha-X-Real-Estate?node-id=3192-6050&node-type=frame&t=0wIEpm7z7DDXDKs5-1&scaling=contain&content-scaling=fixed&page-id=2494%3A49756&starting-point-node-id=3192%3A6050"
+    },
+    {
+        w_no:5,
+        w_name:"Swivel",
+        w_img:project5_img,
+        w_desc: "Pioneered the vision of a fashion mobile app that employs AI",
+        w_url: "https://www.figma.com/proto/5xGoSng8hSRgq9z6b9Y0Dw/AIGC-Swivel?node-id=4-2&node-type=frame&t=Al3dxKODmIzQcKAU-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+
+    },
+    // {
+    //     w_no:6,
+    //     w_name:"Web design",
+    //     w_img:project6_img,
+    //     w_desc: "Designed an original B2B web app for DrayEasy"
+    //
+    // },
+]
+ 
+export default mywork_data;
